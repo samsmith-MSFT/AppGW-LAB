2b192c0523499727a8169ce6d10a8ecd175f1667
samsmith <samsmith@brittandsam.com>
2024-10-23 15:17:51 -0500

    new file:   StartFWandAppGW.ps1

---
+++ StopFWandAppGW.ps1
@@ -0,0 +1,10 @@
+$answers = Get-Content -Raw -Path "answers.json" | ConvertFrom-Json
+
+# Stop AppGW
+$AppGw = Get-AzApplicationGateway -Name "my-app-gateway" -ResourceGroupName $answers.resourceGroupName
+Stop-AzApplicationGateway -ApplicationGateway $AppGw
+
+# Stop Azure Firewall
+$azfw = Get-AzFirewall -Name "azfw-hub" -ResourceGroupName $answers.resourceGroupName
+$azfw.Deallocate()
+Set-AzFirewall -AzureFirewall $azfw
